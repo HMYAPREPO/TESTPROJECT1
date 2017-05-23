2330c346b06f913c89d82727b3e4cd19c15752eb
Hoay <hoayyap@gmail.com>
2017-05-23 11:30:57 -0700

    commit from git mac
    git mac

---
+++ TestProduct/TestProduct.h
@@ -0,0 +1,19 @@
+//
+//  TestProduct.h
+//  TestProduct
+//
+//  Created by Macbook on 5/19/17.
+//  Copyright © 2017 Ghoststream. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestProduct.
+FOUNDATION_EXPORT double TestProductVersionNumber;
+
+//! Project version string for TestProduct.
+FOUNDATION_EXPORT const unsigned char TestProductVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestProduct/PublicHeader.h>
+
+
